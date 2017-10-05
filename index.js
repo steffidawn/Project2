6384ae9446194e9377aa32631b651eeeadaa10c2
hsg2007@yahoo.de <YOUR-EMAIL-ADDRESS>
2017-10-05 11:34:30 -0700

    Fixing more ejs stuff from yesterday

--- index.js
+++ index.js
@@ -3,16 +3,12 @@ require('dotenv').config();
 var express = require('express');
 var bodyParser = require('body-parser');
 var ejsLayouts = require('express-ejs-layouts');
-// var rowdy = require('rowdy-logger');
 var session = require('express-session');
 var flash = require('connect-flash');
 var isLoggedIn = require('./middleware/isLoggedIn');
 var db = require('./models');
 
-
 var app = express();
-
-// var rowdyResults = rowdy.begin(app);
 
 app.set('view engine', 'ejs');
 
@@ -28,17 +24,14 @@ app.use(session({
 app.use(flash());
 
 app.use(function(req, res, next) {
-  // before every route, attach the flash messages and current user to res.locals
   res.locals.alerts = req.flash();
   res.locals.currentUser = req.user;
   next();
 });
 
 var passport = require('./config/ppConfig');
-// initialize the passport configuration and session as middleware
 app.use(passport.initialize());
 app.use(passport.session());
-//Bringing in volunteer,org, profile, and org controllers:
 app.use('/auth', require('./controllers/auth'));
 app.use('/orgs', require('./controllers/orgs'));
 app.use('/volunteers', require('./controllers/volunteers'));
@@ -46,29 +39,27 @@ app.use('/profile', require('./controllers/profile'));
 
 
 //Get routes:
-app.get('/', function(req,res){
-  res.send('<h1>Here is some HTML</h1>');
-});
-// app.get('/', function(req, res) {
-//   res.render('index');
-// });
 
+// app.get('/', function(req, res){
+//   res.send(`
+//     <h1>Here is some HTML</h1>
+//     `);
+// });
+app.get('/', function(req, res) {
+  // db.volunteer.findAll({
+  //   include: [db.author]
+  // }).then(function(posts) {
+    res.render('./main/index');
+    // , { volunteers: volunteers }
+  });
+  // catch(function(error) {
+  //   res.status(400).render('main/404');
+  // });
 
 app.get('/profile', isLoggedIn, function(req, res) {
   res.render('profile');
 });
 
-
-
-
-
-
-
-
-
 var server = app.listen(process.env.PORT || 3000);
 
-// , function(){
-//   rowdyResults.print();
-// }
 module.exports = server;
