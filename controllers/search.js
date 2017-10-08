7c0059b5562fe0ff2d8b6688b43af10e01f2cc17
hsg2007@yahoo.de <YOUR-EMAIL-ADDRESS>
2017-10-08 14:46:28 -0700

    Trying to resolve API issues

--- controllers/search.js
+++ controllers/search.js
@@ -5,21 +5,29 @@ var bodyParser = require('body-parser');
 var passport = require('../config/ppConfig');
 var request = require('request');
 
-router.get("/", function(req, res) {
-    res.render('search/index')
+router.get('/search', function(req, res, next){
+  var orgApi = 'https://projects.propublica.org/nonprofits/api/v2/search.json?q=propublica';
+  res.render('orgApi', {organizations: organizations});
 });
-
-router.post("/results", function(req, res) {
-    var orgApi = 'https://projects.propublica.org/nonprofits/api/v2/search.json?q=propublica';
-    var q = req.body.name;
-    request(orgApi + q, function(error, response, body) {
-        if (error) {
-            return res.send(error);
-        }
-        var data = JSON.parse(body);
-        var results = data.data
-        res.render('search/results', { results: results })
-    });
-});
+// router.get("/", function(req, res) {
+//     res.render('search/index')
+// });
+//
+// router.post('/search', function(req, res){
+//
+// })
+// router.post("/results", function(req, res) {
+//     var orgApi = 'https://projects.propublica.org/nonprofits/api/v2/search.json?q=propublica';
+//     console.log('orgApi');
+//     var q = req.body.name;
+//     request(orgApi + q, function(error, response, body) {
+//         if (error) {
+//             return res.send(error);
+//         }
+//         var data = JSON.parse(body);
+//         var results = data.data
+//         res.render('search/results', { results: results })
+//     });
+// });
 
 module.exports = router;
